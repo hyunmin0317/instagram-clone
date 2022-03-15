f724e87adbbb4d198aea2ecf98c7cdea5b846c75
hyunmin <choihm9903@naver.com>
2022-03-15 11:49:40 +0900

    feat: main-page

--- src/main/java/com/clone/instagram/web/controller/IndexController.java
+++ src/main/java/com/clone/instagram/web/controller/IndexController.java
@@ -1,13 +1,8 @@
 package com.clone.instagram.web.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @Controller
@@ -28,6 +23,6 @@ public class IndexController {
     // 메인 화면으로 이동
     @GetMapping({"/", "post/story"})
     public String story() {
-        return "post/story";
+        return "post/home";
     }
 }
